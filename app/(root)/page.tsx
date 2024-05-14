06806fe81d98a8c58d5dbc444f6447b800583af2
Prathamesh-nair <104072368+Prathamesh-nair@users.noreply.github.com>
2024-05-15 00:36:52 +0530

    implementing right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import HeaderBox from "./../../components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 
 const Home = () => {
-  return <div>Home</div>;
+  const loggedIn = { firstName: "Prathamesh", lastName: "Nair", email: "prathamesh@email.com" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          ></TotalBalanceBox>
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
+    </section>
+  );
 };
 
 export default Home;
